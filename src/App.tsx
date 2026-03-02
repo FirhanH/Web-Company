9a540b1dde77b25d8698f9f64af4c9d755ce3fd3
Firhan Hafiansyah <122663027+FirhanH@users.noreply.github.com>
2026-03-02 12:55:13 +0700

    Remove routing and add test message
    Removed routing components and added a test message.

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,7 @@
-import { HashRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import About from './pages/About'
-import Services from './pages/Services'
-import Projects from './pages/Projects'
-import Contact from './pages/Contact'
-
 export default function App() {
   return (
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </HashRouter>
+    <div style={{ color: "red", fontSize: "40px" }}>
+      TEST MUNCUL
+    </div>
   )
 }
