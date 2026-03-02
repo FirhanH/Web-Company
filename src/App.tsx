3fd4b1b4f39ccb976e4f7ca17faf7a0e1ca14ee6
Firhan Hafiansyah <122663027+FirhanH@users.noreply.github.com>
2026-03-02 13:02:51 +0700

    Set up routing with React Router in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Services from './pages/Services'
+import Projects from './pages/Projects'
+import Contact from './pages/Contact'
+
 export default function App() {
   return (
-    <div style={{ color: "red", fontSize: "40px" }}>
-      TEST MUNCUL
-    </div>
+    <BrowserRouter basename="/Web-Company/">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
